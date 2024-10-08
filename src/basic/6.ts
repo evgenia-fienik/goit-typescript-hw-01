fdc206bd82a9f552b8e399c7bc6e18717ee16f9e
evgenia-fienik <160489180+evgenia-fienik@users.noreply.github.com>
2024-10-08 09:25:53 +0300

    Initial commit

---
+++ src/basic/6.ts
@@ -0,0 +1,28 @@
+interface Address {
+    city: string;
+    country: string;
+}
+
+interface Person { 
+    name: string;
+    age: number;
+    email: string;
+    address ?: Address;
+}
+
+
+const mango = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com',
+    address: {
+        city: 'New York',
+        country: 'USA'
+    }
+};
+
+const poly = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com'
+};
